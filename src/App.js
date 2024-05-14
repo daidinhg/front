b7e85f999fc4cb1bca887f7f6312a28dfd1a82cc
daidinhg <kecodoc0909@gmail.com>
2024-05-14 19:49:15 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ function App() {
   const fetchUserDetails = async()=>{
       const dataResponse = await fetch(SummaryApi.current_user.url,{
         method : SummaryApi.current_user.method,
-        credentials : 'include'
+        credentials : 'include',
+        headers: {
+          'Content-Type': 'application/json'
+      },
       })
 
       const dataApi = await dataResponse.json()
@@ -31,7 +34,10 @@ function App() {
   const fetchUserAddToCart = async()=>{
     const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
       method : SummaryApi.addToCartProductCount.method,
-      credentials : 'include'
+      credentials : 'include',
+      headers: {
+        'Content-Type': 'application/json'
+    },
     })
 
     const dataApi = await dataResponse.json()
